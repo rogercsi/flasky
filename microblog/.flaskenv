960ae339bce3e3039d11f729d9bc08cca2727f5b
ZOU <rogercsizou@icloud.com>
2018-12-12 10:30:02 +0800

    8th try-email-debug

--- microblog/.flaskenv
+++ microblog/.flaskenv
@@ -1,1 +1,7 @@
 FLASK_APP=microblog.py
+FLASK_DEBUG=1
+MAIL_PASSWORD="harvardZOU0727"
+MAIL_PORT="587"
+MAIL_SERVER="smtp.googlemail.com"
+MAIL_USERNAME="rogercsizou@gmail.com"
+MAIL_USE_TLS="1"
--- microblog/config.py
+++ microblog/config.py
@@ -7,3 +7,11 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['flask_test_2018@163.com']
+    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 25
